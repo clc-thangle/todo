932fe58e254242420fe8febfc71df2249426918f
Le Nho Thang <gianggiang1511@gmail.com>
2020-10-31 01:03:44 +0700

    edit

--- src/App.js
+++ src/App.js
@@ -5,44 +5,57 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            item : []
-        } 
+            item: [],
+            newItem: []
+        }
         this.addItem = this.addItem.bind(this);
         this.deleteItem = this.deleteItem.bind(this);
     }
-    
+
     addItem = (taskName) => {
         const newTask = {
-            text : taskName,
-            key : Date.now()
+            text: taskName,
+            key: Date.now()
         }
-        if( taskName !== null && taskName !=='')
-        {
-            const item = [...this.state.item,newTask]
-            this.setState( state => {
-                return{
-                    item: [...state.item,newTask]
+        if (taskName !== null && taskName !== '') {
+            const item = [...this.state.item, newTask]
+            this.setState(state => {
+                return {
+                    item: [...state.item, newTask]
                 }
             })
             console.log(item);
         }
     }
 
+    editItem = (key, value) => {
+        const index = this.state.item.findIndex((mang) => {
+            return mang.key = key;
+        });
+        const tam = Object.assign({}, this.state.item[index]);
+        const todoItems = Object.assign({}, this.state.item);
+        todoItems[index] = {
+            ...tam,
+            text: value 
+        }
+        this.setState({todoItems})
+    }
+
     deleteItem(key) {
         var filteredItems = this.state.item.filter(function (items) {
-          return (items.key !== key);
+            return (items.key !== key);
         });
-       
+
         this.setState({
-          item: filteredItems
+            item: filteredItems
         });
-      }
+    }
 
     render() {
         return (
             <div>
-                <TodoList addItem={this.addItem}/>
-                <TodoItem entris={this.state.item} delete={this.deleteItem}/>
+                <TodoList addItem={this.addItem} />
+                <TodoItem editItem={(key,value) => this.editItem(key,value)} entris={this.state.item} delete={this.deleteItem} />
             </div>
         );
     }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
 
-class TodoItem extends Component {    
+class TodoItem extends Component {
 
     delete(key) {
         this.props.delete(key);
     }
 
     createTask = item => {
-        return(
+        return (
             <div>
-            <input key={item.key}>
-                {item.text}
-            </input>
-            <button onClick={() => this.delete(item.key)} className="delete">Delete</button>
+                <li key={item.key}>
+                    <input onChange={() => this.props.editItem(item.key)} value={item.text}>
+                        
+                    </input>
+                </li>
+                <button onClick={() => this.delete(item.key)} className="delete">Delete</button>
             </div>
-            
+
         )
     }
 
     render() {
-    const listItems = this.props.entris.map(this.createTask);
+        const listItems = this.props.entris.map(this.createTask);
         return (
             <ul className="theList">{listItems}</ul>
         );
